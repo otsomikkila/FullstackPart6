792f30614a7c02da1f146f7a2a13dfe5591e1bb9
otsomikkila <otso.mikkila@hotmail.fi>
2025-01-16 16:39:35 +0200

    Exercise 6.21

--- query-anecdotes/src/App.jsx
+++ query-anecdotes/src/App.jsx
@@ -1,17 +1,17 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 import { useQuery } from '@tanstack/react-query'
-import axios from 'axios'
+import { getAnecdotes } from './requests'
+
 
 const App = () => {
-
   const handleVote = (anecdote) => {
     console.log('vote')
   }
 
   const result = useQuery({
     queryKey: ['anecdotes'],
-    queryFn: () => axios.get('http://localhost:3001/anecdotes').then(res => res.data),
+    queryFn: getAnecdotes,
     retry: 1
   })
 
@@ -22,7 +22,7 @@ const App = () => {
     return <div>anecdote service not available due to problems in server</div>
   }
   const anecdotes = result.data
-  console.log(anecdotes)
+  //console.log(anecdotes)
   //console.log(JSON.parse(JSON.stringify(result)))
 
 
---
+++ query-anecdotes/src/requests.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+const baseURL = 'http://localhost:3001/anecdotes'
+
+export const getAnecdotes = () => 
+  axios.get(baseURL).then(res => res.data)
+
+export const createAnecdote = newAnecdote => {
+  axios.post(baseURL, newAnecdote).then(res => res.data)
+}
