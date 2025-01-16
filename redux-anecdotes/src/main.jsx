199055559374bc93bcb4abf32e9096053766b6e8
otsomikkila <otso.mikkila@hotmail.fi>
2025-01-16 10:22:49 +0200

    Anecdotes and the Backend, step 1

--- redux-anecdotes/src/main.jsx
+++ redux-anecdotes/src/main.jsx
@@ -3,7 +3,6 @@ import { Provider } from 'react-redux'
 import { store } from './store'
 import App from './App'
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,26 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
-const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
-
-const initialState = anecdotesAtStart.map(asObject)
-
 const generateId = () =>
   Number((Math.random() * 1000000).toFixed(0))
 
@@ -30,7 +9,7 @@ const orderAnecdotes = ( list ) => {
 
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
-  initialState,
+  initialState: [],
   reducers: {
     createAnecdote(state, action) {
       const content = action.payload
@@ -50,9 +29,12 @@ const anecdoteSlice = createSlice({
       return orderAnecdotes(state.map(note =>
         note.id !== id ? note : changedAnecdote 
       ))
+    },
+    setAnecdotes(state, action) {
+      return action.payload
     }
   }
 })
 
-export const { createAnecdote, vote } = anecdoteSlice.actions
+export const { createAnecdote, vote, setAnecdotes } = anecdoteSlice.actions
 export default anecdoteSlice.reducer
